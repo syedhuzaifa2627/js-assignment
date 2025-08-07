786e274e6ea665cfe3dfa20d162bfc8e00d12255
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-08-07 21:00:53 +0500

    second commit

--- app.js
+++ app.js
@@ -1,80 +1,251 @@
-// Chapter 1
+// Chapter 4
 
-//alert("Welcome to Js World")
+// Question 1
 
-//alert("Error! Please ener a valid password.")
+// var fullName = "syed" , midName ="Huzaifa" , lastName ="Tauseef"
 
-//alert("Hi... I can run Js through my web browser's console.")
+// Question 2
 
+// var myName = "Syed Huzaifa Tauseef"
+// var my_age = 17
+// var $income = 20000
+// var _city = "Karachi"
+// var number1 = 10
 
+// var my-Name = "Syed Daniyal Haider"
+// var £age = 13
+// var 1colour = "red"
+// var const = 89
+// var myUser Name = "dani07"
 
- //Chapter 2
+// Question 3
 
- var username = ("huzaifasyed2627")
+// document.writeln("<h1> Rules for Naming JS variables. </h1>")
 
- var myName = ("Syed Huzaifa Tauseef")
- var Message = ("Hello World!")
+// var rule1 = "Variable names can only contain numbers, $ and _.  For example : $my_1stVariable.<br>"
 
- alert (Message)
+// var rule2 = "Variables must begin with a letter, $ or _. For example $name, _name or name. <br>"
 
+// var rule3 = "Variables names are case sensitive. <br>"
 
- var fName = ("Fede Valverde")
-
- alert (fName)
-
- var age =("20 years old")
- alert(age)
-
- var accupation =("international footballer")
- alert(accupation)
-
-
- var myPizza = ("PIZZA \n PIZZ \n PIZ \n PI \n P")
-
- alert (myPizza)
-
-
- var eMail = ("huzaifasyed2627@gmail.com")
- var note = ("My Email Address is ")
- alert ( note + eMail)
-
+// var rule4 = "Variables names should not be Js keywords."
  
- var book = ('I am trying to learn from the A Smarter way to Javascript')
-
-  alert (book)
+//  document.writeln(rule1 + rule2 + rule3 + rule4)
 
 
- var ohYes = ("Yah! I can write HTML content through Javascript")
+// Chapter 5
+
+// Question 1 and 2
+
+// var num1 = 6
+// var num2 = 3
+// var newNum = num1 + num2
+
+// var nextNum = num1 - num2
+
+// var mulNum = num1 * num2
+
+// var modNum = num1 % num2
+
+// document.writeln("Sum of " + num1 + " and " + num2 + " is " + newNum + ". <br>")
+// document.writeln("Difference of " + num1 + " and " + num2 + " is " + nextNum + ". <br>")
+// document.writeln("Product of " + num1 + " and " + num2 + " is " + mulNum+ ". <br>")
+// document.writeln("Modulus of " + num1 + " and " + num2 + " is " + modNum+ ". <br>")
+
+// Question 3
+ 
+//var num;
+//document.writeln("Value after variable declaration is: " + num + ". <br>")
+
+//num = 8
+//document.writeln("Initial value: " + num + ". <br>")
+
+//inNum = ++num
+//document.writeln("Value after increament is: " + inNum + ". <br>")
+
+//addNum = inNum + 7
+//document.writeln("Value after addition is: " + addNum + ". <br>")
+
+//decNum = --addNum
+//document.writeln("Value after decreament is: " + decNum + ". <br>")
+
+//modNum = decNum % 3
+//document.writeln("The remainder is: " + modNum + ". <br>")
 
 
- document.writeln (ohYes)
+// Question 4
+
+//var ticketCost = 500
+//var ticketQuan = 4
+
+//var totalCost = ticketCost * ticketQuan
+
+//document.writeln("Total cost to buy " + ticketQuan + " tickets to a movie is " + totalCost + "PKR") 
+
+//Question 5
+
+//var constNum = 6
+
+//document.writeln("<h2> Table of " + constNum + "</h2>")
 
 
- var theEnd = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+//document.writeln(constNum + " x 1 = " + (constNum* 1) + "<br>");
+//document.writeln(constNum + " x 2 = " + (constNum * 2) + "<br>");
+//document.writeln(constNum + " x 3 = " + (constNum * 3) + "<br>");
+//document.writeln(constNum + " x 4 = " + (constNum * 4) + "<br>");
+//document.writeln(constNum + " x 5 = " + (constNum * 5) + "<br>");
+//document.writeln(constNum + " x 6 = " + (constNum * 6) + "<br>");
+//document.writeln(constNum + " x 7 = " + (constNum * 7) + "<br>");
+//document.writeln(constNum + " x 8 = " + (constNum * 8) + "<br>");
+//document.writeln(constNum + " x 9 = " + (constNum * 9) + "<br>");
+//document.writeln(constNum + " x 10 = " + (constNum * 10) + "<br>");
+
+// Question 6
+
+//var celsius = 50
+
+//var fahrenheit = (celsius * 9/5 ) + 32
+
+//document.writeln(celsius + "°C is " + fahrenheit + "°F <br>")
+
+//var newfahrenheit = 85
+
+//var newCelsius = (newfahrenheit - 32) * 5/9
+
+//document.writeln(newfahrenheit + "°F is " + newCelsius + "°C")
 
 
- alert (theEnd)
+// Question 7
 
-// Chapter 3 
+//document.writeln("<h1> Shopping Cart </h1>")
 
- //var myAge = ("I am 18 years old");
+//let priceItem1 = 900
+//let priceItem2 = 600
+//let quanItem1 = 5
+//let quanItem2 = 7
+//var shippinCharges = 200
 
- //alert (myAge)
+//var totalItem1 = priceItem1 * quanItem1 
+//var totalItem2 = priceItem2 * quanItem2
 
- //var timeLine = ("you have visited this site 14 times");
+//var totalCost = totalItem1 + totalItem2 + shippinCharges
+
+//document.writeln("Price of item 1 is " + priceItem1 + ".<br>")
+//document.writeln("Price of item 2 is " + priceItem2 + ".<br>")
+//document.writeln("Quantity of item 1 is " + quanItem1 + ".<br>")
+//document.writeln("Quantity of item 2 is " + quanItem2 + ".<br>")
+//document.writeln("Shipping charges: " + shippinCharges + ".<br> <br>")
+
+//document.writeln("Total cost of your order is " + totalCost + ".<br>")
+
+// Question 8
+
+//document.writeln("<h1> Marks Sheet </h1>")
+
+//let totalMarks = 980
+//let marksObtained = 920
+//let percentage = (marksObtained * 100) / totalMarks
+
+//document.writeln("Total Marks: " + totalMarks + ".<br>")
+//document.writeln("Marks Obtaied: " + marksObtained + ".<br>")
+//document.writeln("Percentage: " + percentage + ".<br>")
+
+// Question 9
+
+// 1 US dollar = 104.80 pakistan rupees
+// 1 Saudi riyal = 28 pakistani rupees
+
+//document.writeln("<h1> Currency in PKR </h1>")
+
+//var usDollar = 10
+//var saudiRiyal = 25
+
+//totalCurrency = (usDollar * 104.80) + (saudiRiyal * 28)
+
+//document.writeln("Total Currency in PKR: " + totalCurrency + ".")
 
 
- //alert (timeLine)
+// Question 10
 
- //var birthYear = ("My birth year is 1947 <br> Data type of my declared variable is strings</br>");
+//let newValue = 15
+
+//let changeValue = (newValue + 5) * 10 / 2
+
+//console.log(changeValue)
+
+// Question 11
+
+//document.writeln("<h1> Age Calculator </h1>")
+
+//let currentYear = 2025
+//let birthYear = 2007
+
+//let yourAge = currentYear - birthYear 
+
+//document.writeln("Current Year: " + currentYear + ".<br>" + "Birth Year: " + birthYear + ".<br>" + "Your Age is: " + yourAge + ".<br>" )
+
+// Question 12
+
+//document.writeln("<h1> The Geometrizer </h1>")
+
+//var circleRadius = 12
+//var circumference = 2 * 3.142 * circleRadius
+//var area = 3.142 * circleRadius * circleRadius
+
+//document.writeln("Radius of a circle is " + circleRadius + "<br>" + "The circumference is " + circumference + "<br>" + "The area is " + area + "<br>")
+
+// Quesion 13
+
+//document.writeln("<h1> The Life Time Supply Calculator </h1>")
+
+//var favSnack = "choclate chip"
+//var age = 21
+//var estimatedAge = 60
+//var amountPerDay = 2
+
+//var expectedSnack = (estimatedAge - age) * amountPerDay
+
+//document.writeln("Favourite Snack: " + favSnack + ".<br>" + "Current Age: " + age + ".<br>" + "Estimated Maximum Age: " + estimatedAge + ".<br>" + "Amount of snack per day: " + amountPerDay + ".<br> <br>")
+
+//document.writeln("You will need " + expectedSnack + " " + favSnack + " to last you until the ripe old age of " + estimatedAge )
 
 
+// Chapter 6
 
- //document.writeln (birthYear)
+// Question 1
 
-//var visitorName = ("Fede Valverde ordered ");
-//var productTitle =  (" Jersey(s)");
-//var quantity = (19);
-//var store = (" on ABC clothing store");
+//document.writeln("<h2> Result </h2>")
+//var repeatLine = "Now the value of a is: "
 
-//document.writeln (visitorName + quantity + productTitle + store);
+//var a = 10
+//document.writeln("The value of a is: " + a + "<br> ..................................... <br> <br>")
+
+//newa = ++a
+//document.writeln("The value of ++a is: " + newa + "<br>" + repeatLine + " " + newa + "<br> <br> <br>")
+
+//nexta = a++ 
+//document.writeln("The value of a++ is: " + nexta + "<br>" + repeatLine + " " + a + "<br> <br> <br>")
+
+//ultraNewa = --a
+//document.writeln("The value of --a is: " + ultraNewa + "<br>" + repeatLine + " " + a + "<br> <br> <br>")
+
+//ultraProNewa = a--
+//document.writeln("The value of a-- is: " + ultraProNewa + "<br>" + repeatLine + " " + a + "<br> <br> <br>")
+
+// Question 2
+
+//var a = 2
+//var b =1
+
+//var result = --a - --b + ++b + b--
+
+//document.writeln("a is " + a + "<br>" + "b is " + b + "<br>")
+//document.writeln("The result is " + result)
+
+// Quesion 3
+
+ var username = prompt("Enter your name")
+
+ alert("Hello" + " " + username + "\n" +"Welcome to my JS world!")
+
+
